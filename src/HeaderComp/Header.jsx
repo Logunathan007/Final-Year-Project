9e514f13de0848c4a87eca12f07ec108504f7d51
Logunathan J <logulogu20032003@gmail.com>
2024-05-20 18:11:32 +0530

    Final commit

--- src/HeaderComp/Header.jsx
+++ src/HeaderComp/Header.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Header.css'
 
 const Header = ({setPageVal,pageVal}) => {
+
+  const [isAllActive, setIsAllActive] = useState(true);
+  const [isFaultyActive, setIsFaultyActive] = useState(false);
+
+  const buttonActive = {
+    backgroundColor: 'blue',
+    color: "white"
+  }
+
+  const buttonInActive = {
+    backgroundColor: 'white',
+    color: "black"
+  }
+  const handleAllClick = () => {
+    setPageVal("all");
+    setIsAllActive(true);
+    setIsFaultyActive(false);
+  }
+
+  const handleFaultyClick = () => {
+    setPageVal("faulty");
+    setIsFaultyActive(true);
+    setIsAllActive(false);
+  }
+
   return (
     <div className='header'>
         <button className='all all-button'
-          onClick={
-            ()=>{
-              setPageVal("all")
-            }
-          }
+          style={isAllActive ? buttonActive : buttonInActive}
+          onClick={handleAllClick}
         >
             All
         </button>
         <button
+          style={isFaultyActive ? buttonActive : buttonInActive}
           className='faulty-button'
-          onClick={
-            ()=>{
-              setPageVal("faulty")
-            }
-          }
+          onClick={handleFaultyClick}
         >
             Faulty
         </button>
